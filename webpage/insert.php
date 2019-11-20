19880979b6ec084bf281414ce16743b4f703144b
Michael Wettstein <chischte@gmx.ch>
2019-11-20 08:18:02 +0100

    Move webpage files to webpage folder

--- webpage/insert.php
+++ webpage/insert.php
@@ -14,7 +14,8 @@ if ($result = $mysqli->query($query)) {
         $field1name = $row["col1"];
         $field2name = $row["col2"];
         $field3name = $row["col3"];
-        
+        $field4name = $row["col4"];
+        $field5name = $row["col5"];
  
         echo '<b>'.$field1name.$field2name.'</b><br />';
         echo $field5name.'<br />';
