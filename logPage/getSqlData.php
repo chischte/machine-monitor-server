c698e4f3d2ccc12ddb26f070dc0dfd06a1d8def0
Michael Wettstein <chischte@gmx.ch>
2019-11-28 22:43:36 +0100

    Add a comment to index.html

--- logPage/getSqlData.php
+++ logPage/getSqlData.php
@@ -1,29 +1,26 @@
 <?php
-
 $con = new mysqli("machinelogger.synology.me", "michi", "uint8_t0-255", "MACHINE_LOG_DB");
 
-if (mysqli_connect_errno())
-{
-echo "Failed to connect to MySQL: " . mysqli_connect_error();
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-$result = mysqli_query($con,"SELECT * FROM machinelog");
+$result = mysqli_query($con, "SELECT * FROM machinelog");
 
 echo "<table border='1'>
 <tr>
 <th>TestRig</th>
 <th>Status</th>
-<th>  Anzahl Zyklen___  </th>
+<th>Anzahl Zyklen</th>
 
 </tr>";
 
-while($row = mysqli_fetch_array($result))
-{
-echo "<tr>";
-echo "<td>" . $row['machine_id'] . "</td>";
-echo "<td>" . $row['machine_status'] . "</td>";
-echo "<td>" . $row['cycles'] . "</td>";
-echo "</tr>";
+while ($row = mysqli_fetch_array($result)) {
+    echo "<tr>";
+    echo "<td>" . $row['machine_id'] . "</td>";
+    echo "<td>" . $row['machine_status'] . "</td>";
+    echo "<td>" . $row['cycles'] . "</td>";
+    echo "</tr>";
 }
 echo "</table>";
 
