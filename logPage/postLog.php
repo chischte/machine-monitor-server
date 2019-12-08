4b8cfd206b0cb110acbc29e394ccc53973f7774c
Michael Wettstein <chischte@gmx.ch>
2019-12-08 18:03:10 +0100

    Experimental upload

--- logPage/postLog.php
+++ logPage/postLog.php
@@ -13,11 +13,6 @@ if ($conn->connect_error) {
     die("Database Connection failed: " . $conn->connect_error);
 }
 
-// Get current date and time
-date_default_timezone_set('Europe/Zurich');
-$d = date("Y-m-d");
-// echo " Date:".$d."<BR>";
-$t = date("H:i:s");
 
 if (! empty($_POST['id']) && ! empty($_POST['status']) && ! empty($_POST['cycr']) && ! empty($_POST['cyct'])) {
     $testrig = $_POST['id'];
@@ -25,12 +20,12 @@ if (! empty($_POST['id']) && ! empty($_POST['status']) && ! empty($_POST['cycr']
     $cyclereset = $_POST['cycr'];
     $cycletotal = $_POST['cyct'];
 
-   //$rigname = 'PTH STANDARD';
-    //$rigid = '1';
+   $rigname = 'PTH STANDARD';
+   $rigid = '1';
 
-//     if (strcomp($testrig, $rigid)==0) { //0 = Strings are equal
-//         $testrig = $rigname;
-//     }
+    if (testrig == $rigid) { //0 = Strings are equal
+         $testrig = $rigname;
+     }
 
     $sql = "INSERT INTO logs (testrig, status, cyclereset, cycletotal)
 		
