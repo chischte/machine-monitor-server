48fde2be93cfe8f849cb8a510623154a19a81fea
Michael Wettstein <chischte@gmx.ch>
2019-12-08 19:39:07 +0100

    Test

--- logPage/view.php
+++ logPage/view.php
@@ -54,12 +54,12 @@ if ($conn->connect_error) {
 <div id="cards" class="cards">
  
 <?php
-$sql = "SELECT id,testrig,status,cyclereset,cycletotal,DATE_FORMAT(`timestamp`, '%a %d-%c-%Y %d:%i:%s') FROM logs ORDER BY id DESC";
+$sql = "SELECT id,testrig,status,cyclereset,cycletotal,DATE_FORMAT(`timestamp`, '%d:%i:%s'), DATE_FORMAT(`timestamp`, '%a %d-%c-%Y')FROM logs ORDER BY id DESC";
 
 if ($result = mysqli_query($conn, $sql)) {
     // Fetch one and one row
     echo "<TABLE id='c4ytable'>";
-    echo "<TR><TH>LOG NO.</TH><TH>TEST RIG</TH><TH>STATUS</TH><TH>CYCLES SINCE RESET</TH><TH>TOTAL CYCLES</TH><TH>TIMESTAMP</TH></TR>";
+    echo "<TR><TH>LOG NO.</TH><TH>TEST RIG</TH><TH>STATUS</TH><TH>CYCLES SINCE RESET</TH><TH>TOTAL CYCLES</TH><TH>TIME</TH><TH>DATE</TH></TR>";
     while ($row = mysqli_fetch_row($result)) {
         echo "<TR>";
         echo "<TD>" . $row[0] . "</TD>";
@@ -68,6 +68,7 @@ if ($result = mysqli_query($conn, $sql)) {
         echo "<TD>" . $row[3] . "</TD>"; //
         echo "<TD>" . $row[4] . "</TD>";
         echo "<TD>" . $row[5] . "</TD>";
+        echo "<TD>" . $row[6] . "</TD>";
         echo "</TR>";
     }
     echo "</TABLE>";
